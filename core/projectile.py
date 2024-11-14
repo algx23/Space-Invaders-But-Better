4fc5bb66151b9420da314aee09b802ffa97744e4
Alex George <george.alex2305@gmail.com>
2024-11-14 11:39:57 +0000

    quality of life change: typing added typing to Projectile class attribtues
    signed of by: me

--- core/projectile.py
+++ core/projectile.py
@@ -15,22 +15,21 @@ class Projectile(GameObject):
         """
         super().__init__("bullet.png", x, y, 20, 25)
 
-        self.velocity = 1
+        self.velocity: int = 1
         self.screen = screen
-        self.x = x
-        self.y = y
-        self.w = 20
-        self.h = 25
+        self.x: int = x
+        self.y: int = y
+        self.w: int = 20
+        self.h: int = 25
 
     def draw(self):
-        # print("drawing..")
+        # print("drawing..") --> this was for debugging purposes
         self.update_position(self.x, self.y)
 
         self.screen.blit(self.image, (self.x, self.y))
         return
 
     # passing in the enemy rect, to check for collision between the projectile and the thing
-
     def travel(self, enemy_rs) -> None:
 
         while self.y > 0:
@@ -40,6 +39,7 @@ class Projectile(GameObject):
             for rect in enemy_rs:
                 if pygame.Rect.colliderect(projectile_r, rect):
                     print(1)
+                    return
 
             self.draw()
             pygame.display.update()
